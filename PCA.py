8396170689fddd25c645094303a0eb7e26a7906a
mathis <mathisguenet99@gmail.com>
2021-02-18 18:19:28 +0100

    Update PCA and toolsData

--- PCA.py
+++ PCA.py
@@ -5,14 +5,15 @@ import seaborn as sns
 
 def PCA(X:np.ndarray):
     #Standardize data  
-    X_cent = X - np.mean(X , axis = 0)
+    X_scaled = (X - np.mean(X , axis = 0))/np.std(X)
 
     # calculating the covariance matrix of the mean-centered data.
-    cov_mat = np.cov(X_cent , rowvar = False)
+    cov_mat = np.cov(X_scaled , rowvar = False)
 
     #Calculating Eigenvalues and Eigenvectors of the covariance matrix
     #Eigen Values is 1D array and Eigen Vectors is ndarray
-    eigen_values , eigen_vectors = np.linalg.eigh(cov_mat)
+    eigen_values , eigen_vectors = np.linalg.eig(cov_mat)
+    print(eigen_values)
     
     #sort the eigen values and eigen vectors in descending order
     sorted_index = np.argsort(eigen_values)[::-1]
@@ -26,19 +27,25 @@ def PCA(X:np.ndarray):
     # Cumulative explained variance
     cumulative_variance_explained = np.cumsum(variance_explained)
     # Visualizing the eigenvalues
-    sns.lineplot(x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21], y=cumulative_variance_explained)
+    sns.lineplot(x = np.arange(20), y=cumulative_variance_explained)
     plt.xlabel("Number of components")
     plt.ylabel("Cumulative explained variance")
     plt.title("Explained variance vs Number of components")
+    plt.show()
 
     #Projection Matrix
-    n_components = 17
+    n_components = 2
     projection_matrix = (eigen_vectors.T[:][:n_components]).T
-    X_pca = np.dot(X, projection_matrix)
+    X_pca = np.dot(X_scaled, projection_matrix)
     
+    if n_components == 2 :
+        #Visualizing the new coordinates
+        df = pd.DataFrame(data = X_pca, columns = ('first principal component', 'second principal component') )
+        print(df)
+        sns.FacetGrid(df).map(plt.scatter,'first principal component', 'second principal component')
+        plt.show()
     return X_pca
 
 if __name__ == "__main__":
     X = np.random.randint(10,50,400).reshape(20,20) 
     components = PCA(X)
-    print(components)
--- toolsData.py
+++ toolsData.py
@@ -14,51 +14,52 @@ import pickle
 import numpy as np
 
 def plotVolatility3DPoint(tab):
-    moyeness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
+    moneyness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
 
     #filling the arrays for the plot's axis (1 dimension)
     X_maturity = []
-    Y_moyeness = []
+    Y_moneyness = []
     Z_volatility = []
     for list_ in tab:
         for i in range(4, len(list_)):
             X_maturity.append(list_[1])
-            Y_moyeness.append(moyeness[i - 4])
+            Y_moneyness.append(moneyness[i - 4])
             Z_volatility.append(list_[i])
 
     
     #plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X_maturity, Y_moyeness, Z_volatility, c = Z_volatility)
+    ax.scatter(X_maturity, Y_moneyness, Z_volatility, c = Z_volatility)
     plt.xlabel("expiry")
     plt.ylabel("strike")
     plt.show()
 
 def plotVolatilitySurface(tab):
-    moyeness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
+    moneyness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
 
     #filling the arrays for the plot's axis (1 dimension)
     X_maturity = []
-    Y_moyeness = []
+    Y_moneyness = []
     Z_volatility = []
     for list_ in tab:
         for i in range(4, len(list_)):
             X_maturity.append(list_[1])
-            Y_moyeness.append(moyeness[i - 4])
+            Y_moneyness.append(moneyness[i - 4])
             Z_volatility.append(list_[i])
     
     #plot
     fig = plt.figure()
     ax = Axes3D(fig)
-    ax.plot_trisurf(X_maturity, Y_moyeness, Z_volatility, cmap = cm.coolwarm)
+    ax.plot_trisurf(X_maturity, Y_moneyness, Z_volatility, cmap = cm.coolwarm)
     plt.xlabel("expiry")
     plt.ylabel("strike")
     plt.show()
 
 def loadVolatility(path, day):
     """
-    Load all the data in a numpy array
+    Load all the volatility for one day in a numpy array
+    Use for plot3D
     """
     #loading data (pickle file)
     unpickled_df = pd.read_pickle(path)
@@ -67,20 +68,48 @@ def loadVolatility(path, day):
     df = df.dropna()
 
     #converting data frame to numpy
-    tab=df.to_numpy()
+    df =df.to_numpy()
     return tab
+
+def loadAllData(path):
+    """
+    Load all the volatility for all day in a numpy array
+    Use for the PCA
+    """
+    #loading data (pickle file)
+    unpickled_df = pd.read_pickle(path)
+    Ndays = len(unpickled_df)
+    data = np.empty((Ndays,399))
+    i = 0
+    for df in unpickled_df:
+        print(df)
+        df = df.drop(columns=['Forwards', 'nCalDays', 'diff Days'])
+        Nrows = len(df)
+        Ncolumns = len(df.columns) - 1
+        data[i] = 0
+
+        df_temp = np.array(df.iloc[:,1:Ncolumns + 1], dtype=np.float_).reshape(-1)
+        #print(df_temp)
+        #print(len(df_temp))
+        data[i] = df_temp
+        i = i + 1
+        if i == 11:
+            return data
+        
+         
+        
 
 def load_volatility_strike(path, day, maturity = 0):
     """
-    Return a 2d numpy array, volatility & moyeness for a given maturity
+    Return a 2d numpy array, volatility & moneyness for a given maturity
     """
     tab = loadVolatility(path, day)
     tab = tab[maturity]
     tab = tab[3:len(tab) - 1]
-    moyeness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
+    moneyness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
     data = np.empty((len(tab),2))
     for i in range(len(tab)):
-        data[i] = np.array([moyeness[i], tab[i]])
+        data[i] = np.array([moneyness[i], tab[i]])
     return data
 
 def load_volatility_strike_maturity(path, day):
@@ -88,40 +117,16 @@ def load_volatility_strike_maturity(path, day):
     data = np.empty((tab.shape[0]*(tab.shape[1] - 2),3))
     i = 0
     for list_ in tab : #for each maturity
-            moyeness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
-            for j in range(len(moyeness)): #for each volatility   
-                data[i] = np.array([moyeness[j], list_[1], list_[3+j]])
+            moneyness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
+            for j in range(len(moneyness)): #for each volatility   
+                data[i] = np.array([moneyness[j], list_[1], list_[3+j]])
                 i = i + 1
     return data
-
-
-def plotVolatilityPoint_strike(tab):
-    moyeness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
-    Z_volatility = []
-    Y_moyeness = []
-    colors = []
-    j = 2
-    for list_ in tab:
-        for i in range(4, len(list_)):
-            Y_moyeness.append(moyeness[i - 4])
-            Z_volatility.append(list_[i])
-            colors.append(2*j)   
-        j = j*j
-        plt.scatter(Y_moyeness, Z_volatility)
-    plt.xlabel("moyeness")
-    plt.ylabel("volatility")
-    plt.show()
-    
-
-def plotVolatilityPoint_maturity(tab):
-    moyeness = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 1.0, 1.02, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2.0, 2.5, 3.0]
 
 if __name__ == "__main__":
     #tab = load_volatility_strike("NKY_clean.pkl", 80)
     #plotVolatilitySurface(tab)
     #plotVolatility3DPoint(tab)
     #plotVolatilityPoint_strike(tab)
-    data = load_volatility_strike_maturity("NKY_clean.pkl", 80)
-    print("done")
-
+    loadAllData("NKY_clean.pkl")
 
